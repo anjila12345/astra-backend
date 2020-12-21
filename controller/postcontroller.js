fc0f2a652a66560d7164cf4f1674f793439523fd
anjila12345 <anjilasubedi95@gmail.com>
2020-12-21 14:15:28 +0545

    Update postcontroller.js

--- controller/postcontroller.js
+++ controller/postcontroller.js
@@ -2,22 +2,12 @@ const post = require("../models/Post")
 
 //function for adding post
 exports.addpost = (req, res) => {
-
-    console.log(req.body)
-    console.log(req.files)
-    req.files.map(function (items) {
-        const Post = new post({
-            user_id: req.body.user_id,
-            title: req.body.title,
-            description: req.body.description,
-        }
-        )
-        Post.save().then(function () {
-            res.send("post has been added")
-        }).catch(function (e) {
-            res.send(e)
-        })
-
+    console.log(req.body.user_id)
+    var mydata = new post(req.body);
+    mydata.save().then(function () {
+        res.send("post has been added")
+    }).catch(function (e) {
+        res.send(e)
     })
 
 }
