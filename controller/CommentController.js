bf776691c74299e8f00cdd9daca1a91c469a329e
anjila12345 <anjilasubedi95@gmail.com>
2021-01-23 21:13:34 +0545

    delete comment

--- controller/CommentController.js
+++ controller/CommentController.js
@@ -1,21 +1,30 @@
 const comment = require("../models/comment")
 //function for comment on post
-exports.addcomment =(req, res) => {
+exports.addcomment = (req, res) => {
 
     console.log(req.body.user_id)
     var mydata = new comment(req.body);
-        mydata.save().then(function( ){
-                res.send("comment has been added")
-            }).catch(function(e){
-                res.send(e)
-            })
-    
+    mydata.save().then(function () {
+        res.send("comment has been added")
+    }).catch(function (e) {
+        res.send(e)
+    })
+
 }
-exports.getCommentByPostId =(req, res) => {
-    comment.find({post_id:req.params.id}).populate('post_id').then(function(findAllcomment) {
-        res.send(findAllcomment).catch(function(e){
+
+exports.getCommentByPostId = (req, res) => {
+    comment.find({ post_id: req.params.id }).populate('post_id').then(function (findAllcomment) {
+        res.send(findAllcomment).catch(function (e) {
             res.json(e)
         })
-      })
+    })
 }
+
+exports.deletecomment = (function (req, res) {
+    comment.findByIdAndDelete(req.params.id).then(function () {
+        res.send("deleted");
+    }).catch(function () {
+        res.send(e)
+    })
+});
 
--- router/comment.js
+++ router/comment.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const commentController = require("../controller/CommentController")
-router.post("/postcomment",commentController.addcomment)
-router.get("/getcommentbypostid/:id",commentController.getCommentByPostId)
+router.post("/postcomment", commentController.addcomment)
+router.get("/getcommentbypostid/:id", commentController.getCommentByPostId)
+router.delete("/deletecomment/:id", commentController.deletecomment)
+
 
 module.exports = router
